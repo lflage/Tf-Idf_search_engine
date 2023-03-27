18a4df3d802b4a884817795801c65d07931dc088
Lucas Lage <lucasflage@hotmail.com>
2023-03-27 12:12:08 +0200

    updated main py file. Look at TODO:

--- softwareAssignment.py
+++ softwareAssignment.py
@@ -4,10 +4,45 @@ Software Assignment
 '''
 
 import sys
-import string, math
+import string
+import math
+import numpy as np
 from lxml import etree
 from stemming.porter2 import stem
 from collections import Counter
+
+
+def pre_process(txt: str) -> list:
+    """Receives a string of text, removes the punctuation, turns text into 
+    lowercase and performs stemming. Returns the input string as a list of 
+    tokens"""
+    txt = txt.translate(str.maketrans('', '', string.punctuation)).lower()
+    txt = list(map(stem, txt.split()))
+    return txt
+
+
+def tf_func(text_list: list) -> dict:
+    """Returns a dictionary containing the word the its term frequency"""
+    return_dict = {}
+    x = Counter(text_list)
+    max_occurence = x.most_common(1)[0][1]
+    x = dict(sorted(x.items()))
+    for word, count in x.items():
+        tf = count/max_occurence
+        return_dict.update({word: tf})
+    return return_dict
+
+
+def idf_func(vocab: dict, corpus_len: int) -> dict:
+    """Function to calculate IDF over vocab dict. The vocab dict is a dict
+    that contains the doc IDs as keys and the text as a list of tokens in its
+    values"""
+    idf_dict = {}
+    for term, count in vocab.items():
+        idf = math.log(corpus_len/count)
+        idf_dict[term] = idf
+    return idf_dict
+
 
 class SearchEngine:
 
@@ -26,6 +61,9 @@ class SearchEngine:
         specification", we will subtract some points as it will be
         impossible for us to test your program automatically!
         '''
+        self.tf_dict = {}
+        self.idf_dict = {}
+        self.tf_idf = {}
         if create == True:
             #  Read files into an lxml etree object
             tree = etree.parse(collectionName+".xml")
@@ -36,18 +74,14 @@ class SearchEngine:
             # A dictionary where key is a word and the value is the nr of docs
             # it appears in
             vocab = {}
-            
-            
-            
+
             for doc in root.iter('DOC'):
                 # initializing and reseting the document text string
                 doc_txt = ""
                 for element in doc.iter('HEADLINE', "P", "TEXT"):
                     doc_txt += element.text.lower()
-                # Removing punctuation
-                doc_txt = doc_txt.translate(str.maketrans('', '', string.punctuation))
-                # Applying stemming to words
-                doc_txt = list(map(stem, doc_txt.split()))
+
+                doc_txt = pre_process(doc_txt)
 
                 # Creating Vocab and increasing the count if word exists
                 for word in set(doc_txt):
@@ -60,52 +94,55 @@ class SearchEngine:
                 corpus[doc.attrib['id']] = doc_txt
             # Sorting the vocabulary dict on keys (words)
             vocab = dict(sorted(vocab.items()))
+
             # Sorting thecorpus dict on keys
             corpus = dict(sorted(corpus.items()))
             # number of documents in the corpus
-            n_docs = len(corpus)
+            self.n_docs = len(corpus)
 
             # IDF
-            self.idf_dict = {}
+            self.idf_dict = idf_func(vocab, self.n_docs)
             with open(collectionName + '.idf', 'w') as file:
-                for term, count in vocab.items():
-                    idf = math.log(n_docs/count)
-                    self.idf_dict[term] = idf
+                for term, idf in self.idf_dict.items():
                     file.write('{}\t{}\n'.format(term, idf))
-            
 
             # TF
-            self.tf_dict = {}
-            with open(collectionName+".tf", "w") as file:
-                for key, text in corpus.items():
-                    x = Counter(text)
-                    # Returns a list of tuples containing pair of values where
-                    # the most frequent term in index 0 and its count in index 1
-                    # To get the 
-                    max_occurence = x.most_common(1)[0][1]
-
-                    for word, count in dict(sorted(x.items())).items():
-                        tf = count/max_occurence
-                        self.tf_dict[key] = {word,tf}
-                        file.write('{}\t{}\t{}\n'.format(key, word, tf))
-            
-            # TF-IDF
-            tf_idf = {}
             for doc_id, text in corpus.items():
-                
+                self.tf_dict[doc_id] = tf_func(text)
 
-            # TODO: Create TF; save to <collectionName>.tf
-            # TODO: Create IDF; save to <collectionName>.idf
-        
-        else:
+            with open(collectionName+".tf", "w") as file:
+                for doc_id, w_tf_pair in self.tf_dict.items():
+                    for word, tf in w_tf_pair.items():
+                        file.write('{}\t{}\t{}\n'.format(doc_id, word, tf))
+
+        if create == False:
             with open(collectionName+'.tf', 'r') as file:
-                self.tf = file.read()
-            with open(collectionName+'.idf', 'r') as file:
-                self.idf = file.read()
+                for line in file.readlines():
+                    doc_id, term, tf = line.split('\t')
+                    if doc_id not in self.tf_dict.keys():
+                        self.tf_dict[doc_id] = {}
+                    else:
+                        self.tf_dict[doc_id].update({term: float(tf)})
+                self.n_docs = len(self.tf_dict)
 
+            with open(collectionName+'.idf', 'r') as file:
+                for line in file.readlines():
+                    term, idf = line.split('\t')
+                    self.idf_dict[term] = float(idf)
+        # TF-IDF
+        self.word_to_ix = {key: value for key, value in zip(self.idf_dict.keys(),
+                                                            range(len(self.idf_dict)))}
+
+        # TODO: change this to itarate over the TF dict {
+        for doc_id, text in corpus.items():
+            self.tf_idf[doc_id] = np.zeros(len(vocab))
+
+            for term in text:
+                self.tf_idf[doc_id][self.word_to_ix[term]
+                                    ] = self.tf_dict[doc_id][term] * self.idf_dict[term]
+        # TODO: }
         pass
-    
-    
+
     def executeQuery(self, queryTerms):
         '''
         Input to this function: List of query terms
@@ -119,8 +156,34 @@ class SearchEngine:
         May be less than 10 documents if there aren't as many documents
         that contain the terms.
         '''
-        pass
-        
+        # pre process query:
+        query_str = " ".join(queryTerms)
+        query = pre_process(query_str)
+
+        # Query TF:
+        query_tf = tf_func(query)
+
+        # Query TF-IDF
+        query_tf_idf = np.zeros(self.n_docs)
+
+        for term in query:
+            query_tf_idf[self.word_to_ix[term]
+                         ] = query_tf[term] * self.idf_dict[term]
+
+        sim_dict = {}
+        for doc_id, value in self.tf_idf.items():
+            top = np.dot(query_tf_idf, value)
+            bottom = np.linalg.norm(query_tf_idf) * np.linalg.norm(value)
+            sim = top/bottom
+            if sim == 0:
+                continue
+            else:
+                sim_dict.update({doc_id: top/bottom})
+
+        sim_dict = sorted(sim_dict.items(), reverse=True,
+                          key=lambda item: item[1])
+        return sim_dict[:10]
+
     def executeQueryConsole(self):
         '''
         When calling this, the interactive console should be started,
@@ -128,7 +191,8 @@ class SearchEngine:
         simply hits enter.
         '''
         pass
-    
+
+
 if __name__ == '__main__':
     '''
     write your code here:
@@ -138,5 +202,5 @@ if __name__ == '__main__':
     '''
     # Example for how we might test your program:
     # Should also work with nyt199501 !
-    searchEngine = SearchEngine("nytsmall", create=True)
+    searchEngine = SearchEngine("nytsmall", create=False)
     print(searchEngine.executeQuery(['hurricane', 'philadelphia']))
