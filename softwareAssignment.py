9c7008fdd366f4a9f0b866e2519042e7fffb185d
Lucas Lage <lucasflage@hotmail.com>
2023-03-27 15:27:39 +0200

    documented functions and class

--- softwareAssignment.py
+++ softwareAssignment.py
@@ -14,21 +14,34 @@ from collections import Counter
 from pprint import pprint
 
 
-def pre_process(txt: str) -> list:
+def pre_process(in_txt: str) -> list:
     """Receives a string of text, removes the punctuation, turns text into 
     lowercase and performs stemming. Returns the input string as a list of 
-    tokens"""
-    txt = txt.translate(str.maketrans('', '', string.punctuation)).lower()
-    txt = list(map(stem, txt.split()))
+    tokens
+    in_txt:<str> - text to be processed in string
+    txt:<list of str> - processed text as a bag-of-words/ list of tokens
+    """
+    in_txt = in_txt.translate(str.maketrans(
+        '', '', string.punctuation)).lower()
+    txt = list(map(stem, in_txt.split()))
     return txt
 
 
 def tf_func(text_list: list) -> dict:
-    """Returns a dictionary containing the word the its term frequency"""
+    """ Returns a dictionary containing the word the its term frequency value
+    as a key value par in a dict
+
+    text_list:<list> -: input text as a list
+    return_dict:<dict> -: key:value -> word:tf
+    """
     return_dict = {}
+    # used Counter dictionary from the standar python lib
     x = Counter(text_list)
+    # uses the most_common() method to the get the top1 most common token
     max_occurence = x.most_common(1)[0][1]
+    # Sorts the dict alphabetically
     x = dict(sorted(x.items()))
+    # We iterate over the dict and get the tf value for every type in the text
     for word, count in x.items():
         tf = count/max_occurence
         return_dict.update({word: tf})
@@ -38,7 +51,12 @@ def tf_func(text_list: list) -> dict:
 def idf_func(vocab: dict, corpus_len: int) -> dict:
     """Function to calculate IDF over vocab dict. The vocab dict is a dict
     that contains the doc IDs as keys and the text as a list of tokens in its
-    values"""
+    values
+
+    vocab:<dict>        -: consists of doc_id:list of tokens in the doc
+    corpus_len:<int>    -: number of documents in the corpus
+    idf_dict:<dict>     -: word:idf pairs
+    """
     idf_dict = {}
     for term, count in vocab.items():
         idf = math.log(corpus_len/count)
@@ -66,9 +84,11 @@ class SearchEngine:
         self.tf_dict = {}
         self.idf_dict = {}
         self.tf_idf = {}
+
         if create == True:
-            #  Read files into an lxml etree object
+            # Read files into an lxml etree object
             tree = etree.parse(collectionName+".xml")
+            # Gets root node so we can iterate over it
             root = tree.getroot()
 
             # A dictionary where key = doc id, value = doc as a list of word ids
@@ -78,7 +98,8 @@ class SearchEngine:
             vocab = {}
 
             for doc in root.iter('DOC'):
-                # initializing and reseting the document text string
+                # initializing the document text string, adds the content from
+                # the tags "HEADLINE", "P" and "TEXT"
                 doc_txt = ""
                 for element in doc.iter('HEADLINE', "P", "TEXT"):
                     doc_txt += element.text.lower()
@@ -99,53 +120,67 @@ class SearchEngine:
 
             # Sorting thecorpus dict on keys
             corpus = dict(sorted(corpus.items()))
-            # number of documents in the corpus
-            self.n_docs = len(corpus)
 
             # IDF
-            self.idf_dict = idf_func(vocab, self.n_docs)
+            self.idf_dict = idf_func(vocab, len(corpus))
+            # Write .idf files
             with open(collectionName + '.idf', 'w') as file:
                 for term, idf in self.idf_dict.items():
                     file.write('{}\t{}\n'.format(term, idf))
 
             # TF
+            # Iterate over the corpus creating a dictionary where key->doc_id
+            # value another dict with the tf values for the document
             for doc_id, text in corpus.items():
                 self.tf_dict[doc_id] = tf_func(text)
-
+            # Write .tf files
             with open(collectionName+".tf", "w") as file:
                 for doc_id, w_tf_pair in self.tf_dict.items():
                     for word, tf in w_tf_pair.items():
                         file.write('{}\t{}\t{}\n'.format(doc_id, word, tf))
 
         if create == False:
-            # TODO: Fix
+            # Read .tf file of specified corpus
             with open(collectionName+'.tf', 'r') as file:
                 for line in file.readlines():
                     doc_id, term, tf = line.split('\t')
+                    # Converts the tf value from str to float
                     tf = float(tf.strip())
 
+                    # Check if the corresponding doc already exists in the
+                    # tf_dict if it doesn't, create the empty dict and update it
+                    # with the current values for term and tf
                     if doc_id not in self.tf_dict.keys():
                         self.tf_dict[doc_id] = {}
                         self.tf_dict[doc_id].update({term: tf})
                     else:
+                        # add the term and its tf value to the corresponding dict
+                        # for this document
                         self.tf_dict[doc_id].update({term: tf})
-                self.n_docs = len(self.tf_dict)
 
+            # Read .idf file of specified corpus
             with open(collectionName+'.idf', 'r') as file:
                 for line in file.readlines():
                     term, idf = line.split('\t')
+
                     self.idf_dict[term] = float(idf.strip())
         # TF-IDF
+        # Created a word:index dict to convert the words into indices in the
+        # tf-idf vectors
         self.word_to_ix = {key: value for key, value in zip(self.idf_dict.keys(),
                                                             range(len(self.idf_dict)))}
 
+        # Iterates over the tf_dict
         for doc_id, term_tf_pair in self.tf_dict.items():
+            # Initializes the vector for the current document with zeros
             self.tf_idf[doc_id] = np.zeros(len(self.idf_dict))
 
+            # iterates over the tf values in the term_tf_pair dicr
             for term, tf in term_tf_pair.items():
+                # pass tf-idf value to tf_idf dict, acess the correct index
+                # by the word_to_ix dict
                 self.tf_idf[doc_id][self.word_to_ix[term]
                                     ] = tf * self.idf_dict[term]
-        #
         pass
 
     def executeQuery(self, queryTerms):
@@ -209,3 +244,5 @@ if __name__ == '__main__':
     # Should also work with nyt199501 !
     searchEngine = SearchEngine("nytsmall", create=True)
     print(searchEngine.executeQuery(['hurricane', 'philadelphia']))
+    searchEngine2 = SearchEngine("nytsmall", create=False)
+    print(searchEngine2.executeQuery(['hurricane', 'philadelphia']))
